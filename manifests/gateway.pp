46a83b488e97ac4af100c39ad68e4a51dfd35007
Jonathan Davies <jpdavs@gmail.com>
2015-01-19 14:26:55 +0000

    gateway.pp: Removed keyingtries option.

--- manifests/gateway.pp
+++ manifests/gateway.pp
@@ -15,7 +15,6 @@ class strongswan::gateway (
   $ike         = '',
   $esp         = '',
   $keyexchange = '',
-  $keyingtries = '',
   $ikelifetime = '',
   $lifetime    = '',
   $margintime  = '',
