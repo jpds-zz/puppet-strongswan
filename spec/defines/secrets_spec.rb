0b8053300ba4b0db105a3f4773dc766fef111631
PhilipHarries <philip.harries.unix.consulting@gmail.com>
2016-02-01 17:27:45 +0000

    [PH] add tests for secrets config

--- spec/defines/secrets_spec.rb
+++ spec/defines/secrets_spec.rb
@@ -5,7 +5,7 @@ describe 'strongswan::secrets', :type => :define do
     'include strongswan'
   end
 
-  context 'default conn settings on a Debian system' do
+  context 'on a Debian system' do
     let(:facts) {
       {
         :id             => 'root',
@@ -15,26 +15,48 @@ describe 'strongswan::secrets', :type => :define do
         :path           => '/usr/sbin:/usr/bin:/sbin:/bin',
       }
     }
-    let(:title) { 'rw' }
-    let :params do
-      {
-        :options => {
-          'ECDSA'  => 'rwKey.der',
-        },
-      } 
+
+    context 'with certificate based authentication' do
+      let(:title) { 'rw' }
+      let :params do
+        {
+          :options => {
+            'ECDSA'  => 'rwKey.der',
+          },
+        } 
+      end
+
+      it { should contain_class('strongswan::params') }
+
+      it {
+        should contain_concat__fragment('ipsec_secrets_secret-rw').with_content(/# Secrets for rw\./)
+      }
+
+      it {
+        should contain_concat__fragment('ipsec_secrets_secret-rw').with_content(/: ECDSA rwKey\.der/)
+      }
     end
 
-    it { should contain_class('strongswan::params') }
+    context 'with pre-shared-key authentication' do
+      let(:title) { 'ABC-XYZ-vpn' }
+      let :params do
+        {
+          'leftid'      => '@ABC',
+          'rightid'     => '@XYZ',
+          'auth'        => 'PSK',
+          'key'         => 'a long string'
+        } 
+      end
+      it { should contain_class('strongswan::params') }
+      it {
+        should contain_concat__fragment('ipsec_secrets_secret-ABC-XYZ-vpn').with_content(/# Secrets for ABC-XYZ-vpn\./)
+      }
 
-    it {
-      should contain_concat__fragment('ipsec_secrets_secret-rw') \
-        .with_content(/# Secrets for rw\./)
-    }
+      it {
+        should contain_concat__fragment('ipsec_secrets_secret-ABC-XYZ-vpn').with_content(/@ABC @XYZ : PSK a long string/)
+      }
+    end
 
-    it {
-      should contain_concat__fragment('ipsec_secrets_secret-rw') \
-        .with_content(/: ECDSA rwKey\.der/)
-    }
   end
 
   context 'example settings on a Solaris system' do
